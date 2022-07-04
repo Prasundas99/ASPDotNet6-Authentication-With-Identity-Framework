d7e1bd8501e06c22a5efbde64ecb6e29bee3b805
prasun.das <Prasun.das@hrvyst-azure.com>
2022-07-04 18:27:08 +0000

    db connection

--- CollegeScoreApp/CollegeScoreApp/Program.cs
+++ CollegeScoreApp/CollegeScoreApp/Program.cs
@@ -1,8 +1,23 @@
+using CollegeScoreApp.Data;
+using AutoMapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
