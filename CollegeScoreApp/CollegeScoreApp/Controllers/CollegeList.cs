d13c7ba0fbc9f7f01569ff66b2d5587f54cb3de7
prasun.das <Prasun.das@hrvyst-azure.com>
2022-07-04 16:25:24 +0000

    feat: rest apis

--- CollegeScoreApp/CollegeScoreApp/Controllers/CollegeList.cs
+++ CollegeScoreApp/CollegeScoreApp/Controllers/CollegeList.cs
@@ -1,6 +1,54 @@
-﻿namespace CollegeScoreApp.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using CollegeScoreApp.DTN;
+using CollegeScoreApp.Data;
+
+namespace CollegeScoreApp.Controllers
 {
-    public class CollegeList
+    [ApiController]
+    [Route("[controller]")]
+    public class CollegeListController : ControllerBase
     {
+        private readonly ILogger<CollegeList> _logger;
+        private readonly AppDbContext _context;
+
+        public CollegeListController(ILogger<CollegeList> logger, AppDbContext context)
+        {
+            _logger = logger;
+            _context = context; 
+        }
+
+        [HttpGet(Name = "CollegeList")]
+        public async Task<ActionResult<List<CollegeList>>> Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<CollegeList>>> Get(int id)
+        {
+            //var college = await 
+            return Ok();
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<List<CollegeList>>> AddCollege(CollegeList college)
+        {
+            return BadRequest();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<List<CollegeList>>> UpdateCollege(CollegeList college)
+        {
+            return BadRequest();
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<CollegeList>>> DeleteCollege(CollegeList college)
+        {
+            return BadRequest();
+        }
+
     }
 }
---
+++ CollegeScoreApp/CollegeScoreApp/DTN/UserAuth.cs
@@ -0,0 +1,8 @@
+﻿namespace CollegeScoreApp.DTN
+{
+    public class UserAuth
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
