3528ac9e60c9f2c624482cda011e7fa2f7491e22
prasun.das <Prasun.das@hrvyst-azure.com>
2022-07-05 11:04:29 +0000

    auth controllers

---
+++ CollegeScoreApp/CollegeScoreApp/Controllers/AccountsController.cs
@@ -0,0 +1,32 @@
+﻿using CollegeScoreApp.DTN;
+using CollegeScoreApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollegeScoreApp.Controllers
+{
+    [ApiController]
+    [Route("auth/[controller]")]
+    public class AccountsController : ControllerBase
+    {
+        public AccountsController()
+        {
+
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(UserAuth request)
+        {
+            if (request == null)
+                return BadRequest("Username or Password is Empty");
+            return Ok();
+        }
+
+
+        [HttpPost("login")]
+        public async Task<ActionResult<User>> Login(UserAuth request)
+        {
+            return Ok();
+        }
+        
+    }
+}
---
+++ CollegeScoreApp/CollegeScoreApp/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CollegeScoreApp.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        [Required]
+        [StringLength(5)]
+        public string Name { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+        public User()
+        {
+            Id = Guid.NewGuid();
+            CreatedOn = DateTime.Now;
+            Name = "UnNamed";
+            
+        }
+    }
+}
